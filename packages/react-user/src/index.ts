6b2ad5d2db1dbc08446617a33fb4757b327e59b2
Andrew Ghostuhin <me@torinasakura.name>
2020-06-01 21:55:44 +0300

    feat(react-user): init
    affects: @atlantis-lab/react-user

---
+++ packages/react-user/src/index.ts
@@ -0,0 +1,8 @@
+export * from './WithouthUser'
+export * from './WithUser'
+export * from './useUser'
+export {
+  Provider as UserProvider,
+  Consumer as UserConsumer,
+  Context as UserContext,
+} from './Context'
---
+++ packages/react-user/src/useUser.ts
@@ -0,0 +1,5 @@
+import React       from 'react'
+
+import { Context } from './Context'
+
+export const useUser = () => React.useContext(Context)
