a8061b383dbbb38f2868dd46f8d3c209e07d452b
TFK70 <buldozik.nikitosik@mail.ru>
2021-01-14 18:36:35 +0500

    feat(react-locale): add test
    affects: @atlantis-lab/react-locale
    
    ISSUES CLOSED: #69

---
+++ packages/react-locale/__tests__/react-locale.test.tsx
@@ -0,0 +1,44 @@
+import Adapter                                              from 'enzyme-adapter-react-16'
+import React                                                from 'react'
+import { configure, render }                                from 'enzyme'
+
+import { LocaleProvider, LocaleConsumer, LocaleContext, LocaleStore, useLocale } from '../src/index'
+
+configure({ adapter: new Adapter() })
+
+describe('Test suit for react-locale', function describer() {
+    test('should return h1 with provided context', function tester() {
+        const ctxStore = new LocaleStore('en', ['en', 'ru'])
+
+        expect(render(
+            <LocaleProvider value={ ctxStore }>
+                <div>
+                    <LocaleConsumer>
+                        { localeStore => <h1>{ `${localeStore.getCurrent()} ${localeStore.getSupported()}` }</h1> }
+                    </LocaleConsumer>
+                </div>
+            </LocaleProvider>
+        ))
+
+    })
+
+    test('should track LocaleStore changes + useLocale', function tester() {
+        const ctxStore = new LocaleStore('en', ['en', 'ru'])
+
+        const App = () => <ProviderComponent />;
+
+        const ProviderComponent = () => <LocaleProvider value={ ctxStore }><Wrapper /></LocaleProvider>;
+
+        const Wrapper = () => <div><ConsumerComponent /></div>;
+
+        const ConsumerComponent = () => {
+            ctxStore.set('ru');
+
+            const [getCurrent, getSupported, setLocale] = useLocale();
+
+            return <h1 className={'consumer'}>{`${getCurrent} ${getSupported}`}</h1>
+        };
+
+        expect(render(<App />).find('.consumer').text()).toBe('ru en,ru')
+    })
+})
