1366f85c9e012e3994421eb0a1435d76d1f7241c
TFK70 <buldozik.nikitosik@mail.ru>
2021-01-15 19:34:02 +0500

    refactor: refactor tests
    affects: @atlantis-lab/react-auth, @atlantis-lab/react-locale, @atlantis-lab/react-modals,
    @atlantis-lab/react-user
    
    -Hoisted jest to the root
    -Replaced modules
    -Edited test files & environment
    
    ISSUES CLOSED: #75

--- packages/react-auth/__tests__/react-auth.test.tsx
+++ packages/react-auth/__tests__/react-auth.test.tsx
@@ -1,38 +1,39 @@
-import Adapter                                              from 'enzyme-adapter-react-16'
 import React                                                from 'react'
-import { configure, render }                                from 'enzyme'
+import TestRenderer                                         from 'react-test-renderer'
 
 import { AuthConsumer, AuthContext, AuthProvider, useAuth } from '../src/index'
-
-configure({ adapter: new Adapter() })
 
 describe('Test suit for react-auth package', function describer() {
   test('should return h1 with provided token using AuthProvider and AuthConsumer', function tester() {
     const tokenStub: string = 'k4Xergan4Sf7ejaD'
 
-    expect(
-      render(
-        <AuthProvider value={tokenStub}>
-          <div>
-            <AuthConsumer>{token => <h1>{token}</h1>}</AuthConsumer>
-          </div>
-        </AuthProvider>
-      ).text()
-    ).toBe(tokenStub)
+    const App = () => (
+      <AuthProvider value={tokenStub}>
+        <div>
+          <AuthConsumer>{token => <h1>{token}</h1>}</AuthConsumer>
+        </div>
+      </AuthProvider>
+    )
+
+    const testRenderer = TestRenderer.create(<App />)
+
+    expect(testRenderer.root.findByType('h1').props.children).toBe(tokenStub)
   })
 
   test('should return h1 with provided token using AuthContext', function tester() {
     const tokenStub: string = 'k4Xergan4Sf7ejaD'
 
-    expect(
-      render(
-        <AuthContext.Provider value={tokenStub}>
-          <div>
-            <AuthContext.Consumer>{token => <h1>{token}</h1>}</AuthContext.Consumer>
-          </div>
-        </AuthContext.Provider>
-      ).text()
-    ).toBe(tokenStub)
+    const App = () => (
+      <AuthContext.Provider value={tokenStub}>
+        <div>
+          <AuthContext.Consumer>{token => <h1>{token}</h1>}</AuthContext.Consumer>
+        </div>
+      </AuthContext.Provider>
+    )
+
+    const testRenderer = TestRenderer.create(<App />)
+
+    expect(testRenderer.root.findByType('h1').props.children).toBe(tokenStub)
   })
 
   test('should return h1 with provided token using useAuth hook', function tester() {
@@ -58,10 +59,8 @@ describe('Test suit for react-auth package', function describer() {
       return <h1 className='token'>{token}</h1>
     }
 
-    expect(
-      render(<App />)
-        .find('.token')
-        .text()
-    ).toBe(tokenStub)
+    const testRenderer = TestRenderer.create(<App />)
+
+    expect(testRenderer.root.findByType('h1').props.children).toBe(tokenStub)
   })
 })
