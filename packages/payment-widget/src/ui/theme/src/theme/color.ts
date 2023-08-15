8c577fdbb938ba4eb8e3d55ff1c08300c71a33ea
Kent-cpu <ruslanmirzaev16621@gmail.com>
2023-08-15 23:40:57 +0700

    feat: added button styling

---
+++ packages/payment-widget/src/ui/theme/src/theme/color.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  button: {
+    light: {
+      backgroundColor: 'rgb(255,255,255)',
+      font: 'rgb(0,0,0)',
+      border: 'rgb(0,0,0)',
+    },
+    dark: {
+      backgroundColor: 'rgb(0,0,0)',
+      font: 'rgb(242,242,242)',
+      border: 'rgb(0,0,0)',
+    }
+  }
+}
