f7b59e5fbd4eb639d0f85c7ac3df0364ea218703
Kent-cpu <ruslanmirzaev16621@gmail.com>
2023-08-20 19:44:47 +0700

    chore: styled-system moved to dev dependies

--- packages/payment-widget/src/index.ts
+++ packages/payment-widget/src/index.ts
@@ -1,3 +1,9 @@
 export { PaymentWidget } from './ui'
-export {PaymentSettings, PaymentStyles, AdditionalFields, Receipt, ReceiptItem} from './interfaces'
+export {
+  PaymentSettings,
+  PaymentStyles,
+  AdditionalFields,
+  Receipt,
+  ReceiptItem,
+} from './interfaces'
 export * from './enums'
