d69d39443fe546be4a95a3560a3c83a238d29bb2
Ilya Frolov <absolem.dev@gmail.com>
2023-12-09 11:29:23 +0400

    fix(wrapper): add displayName property

--- packages/payment-widget/src/utils/is-custom-element.util.ts
+++ packages/payment-widget/src/utils/is-custom-element.util.ts
@@ -1,11 +1,11 @@
-import { ReactNode }             from 'react'
-import { JSXElementConstructor } from 'react'
-import { isValidElement }        from 'react'
+import { FunctionComponent }    from 'react'
+import { ReactNode }            from 'react'
+import { isValidElement }       from 'react'
 
-import { NameWrapperComponent }  from '../enums'
+import { NameWrapperComponent } from '../enums'
 
 export const isCustomElement = (nameNode: NameWrapperComponent, node: ReactNode): boolean =>
-  isValidElement(node)
-    ? (node.type as JSXElementConstructor<any>).name === nameNode &&
+  isValidElement(node) && typeof node.type === 'function'
+    ? (node.type as FunctionComponent).displayName === nameNode &&
       typeof node.props.children === 'function'
     : false
